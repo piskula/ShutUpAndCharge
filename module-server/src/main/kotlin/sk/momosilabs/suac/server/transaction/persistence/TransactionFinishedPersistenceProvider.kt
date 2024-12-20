bdf699f40d0a750c54ab09ed5712530a39dfdb31
Ondrej Oravčok <Ondrej.Oravcok@cloudflight.io>
2024-12-20 23:48:13 +0100

    fix broken sorting

--- module-server/src/main/kotlin/sk/momosilabs/suac/server/transaction/persistence/TransactionFinishedPersistenceProvider.kt
+++ module-server/src/main/kotlin/sk/momosilabs/suac/server/transaction/persistence/TransactionFinishedPersistenceProvider.kt
@@ -1,12 +1,15 @@
 package sk.momosilabs.suac.server.transaction.persistence
 
 import com.querydsl.core.QueryResults
+import com.querydsl.core.types.Order
+import com.querydsl.core.types.OrderSpecifier
 import com.querydsl.jpa.JPQLQuery
 import com.querydsl.jpa.impl.JPAQueryFactory
 import jakarta.persistence.EntityManager
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 import sk.momosilabs.suac.server.account.persistence.repository.AccountRepository
@@ -49,12 +52,27 @@ open class TransactionFinishedPersistenceProvider(
     }
 
     private fun <T> JPQLQuery<T>.applyPagination(pageable: Pageable) =
-        offset(pageable.offset).limit(pageable.pageSize.toLong())
+        offset(pageable.offset)
+            .limit(pageable.pageSize.toLong())
+            .orderBy(pageable.sort.toQueryDslOrderBy())
 
     private fun <T, U> QueryResults<T>.toPageResult(pageable: Pageable, mapper: (T) -> U): Page<U> = PageImpl(
         results.map { mapper.invoke(it) },
         pageable,
         total,
     )
+
+    private fun Sort.toQueryDslOrderBy(): OrderSpecifier<*> {
+        val orderBy = if (isSorted) this.get().findFirst().get() else Sort.Order.desc("id")
+        val expression = when (orderBy.property) {
+            "time" -> transaction.time
+            "account.lastName" -> transaction.account.lastName
+            "stationId" -> transaction.stationId
+            "kwh" -> transaction.kwh
+            "price" -> transaction.price
+            else -> transaction.id
+        }
+        return OrderSpecifier(if (orderBy.isAscending) Order.ASC else Order.DESC, expression)
+    }
 
 }
